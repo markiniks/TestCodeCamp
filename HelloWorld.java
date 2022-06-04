c4481b3915fbc3e031290228bbdcda4474273cc7
markiniks <mark.iniks@gmail.com>
2022-06-04 08:11:33 +0100

    added to first java project-Hello world

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Testers");
+        System.out.println("Welcome to Cohort 10.0");
+        System.out.println("Yor are doing well");
+        System.out.println("Team Github");
+        System.out.println("keep the winning spirit high");
+        System.out.println("We Move");
+    }
+}
